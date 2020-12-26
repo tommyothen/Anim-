027acf26bb853329a64721547d24c32d20c367e6
DaSushiAsian <42751764+DaSushiAsian@users.noreply.github.com>
2020-12-26 22:31:12 +0000

    Did not need preload

--- preload.js
+++ preload.js
@@ -1,12 +1,13 @@
-// All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
-});
+// It has the same sandbox as a Chrome extension.
+setInterval(() => {
+  if (window.location.href.includes("https://animixplay.to/v1")) {}
+}, 1e2);
+
